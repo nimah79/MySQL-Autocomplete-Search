547a52156336b5339e8c3eaa1eadd0fc3829ca98
Nima HeydariNasab <nima.heydari79@yahoo.com>
2020-08-19 14:04:04 +0430

    Add initial project

---
+++ README.md
@@ -0,0 +1,86 @@
+# MySQL Autocomplete Search
+
+This is a simple and efficient implementation of autocomplete. The main idea is from [prefixy/prefixy]([https://github.com/prefixy/prefixy](https://github.com/prefixy/prefixy)) It shows suggestions sorted by scores and it also supports multitenancy.
+
+An example of autocomplete is Google search field:
+
+![Google autocomplete](images/google_autocomplete.gif)
+
+## Requirements
+
++ MySQL 5+
+
+## Installation
+
+Import `initialize.sql` into your database. You can also import `seed.sql` for demo data.
+
+## Database schema
+
+### Table `apps`
+
+| Key_name | Column_name | Non_unique | Index_type  |
+|----------|-------------|------------|-------------|
+| PRIMARY  | id          |          0 | BTREE       |
+| app_key  | app_key     |          0 | BTREE       |
+
+#### Indexes:
+
+| Non_unique | Key_name | Column_name | Index_type |
+|------------|----------|-------------|------------|
+|          0 | PRIMARY  | id          | BTREE      |
+|          0 | app_key  | app_key     | BTREE      |
+
+### Table `prefixes`
+
+| Field  | Type         | Null | Key | Default | Extra          |
+|--------|--------------|------|-----|---------|----------------|
+| id     | int(11)      | NO   | PRI | NULL    | auto_increment |
+| app_id | int(11)      | NO   | MUL | NULL    |                |
+| prefix | varchar(255) | NO   | MUL | NULL    |                |
+
+
+#### Indexes
+
+| Key_name      | Column_name         | Non_unique | Index_type |
+|---------------|---------------------|------------|------------|
+| PRIMARY       | id                  |          0 | BTREE      |
+| app_id-prefix | app_id, prefix      |          0 | BTREE      |
+| prefix        | prefix              |          1 | BTREE      |
+
+### Table `completions`
+
+| Field      | Type         | Null | Key | Default | Extra          |
+|------------|--------------|------|-----|---------|----------------|
+| id         | int(11)      | NO   | PRI | NULL    | auto_increment |
+| prefix_id  | int(11)      | NO   | MUL | NULL    |                |
+| completion | varchar(255) | NO   |     | NULL    |                |
+| rank       | int(11)      | NO   |     | NULL    |                |
+
+#### Indexes
+
+| Key_name             | Column_name             | Non_unique | Index_type |
+|----------------------|-------------------------|------------|------------|
+| PRIMARY              | id                      |          0 | BTREE      |
+| prefix_id-completion | prefix_id, completion   |          0 | BTREE      |
+| prefix_id            | prefix_id               |          1 | BTREE      |
+
+## Usage
+
+### Inserting a new completion
+
+Use `InsertCompletion(app_id, completion_text, max_prefixes, bucket_size_limit)`. Example:
+
+```sql
+CALL InsertCompletion(1, 'nima', 15, 15);
+```
+
+**Time complexity:** O((max_prefixes × log(table_prefixes_count)) + log(table_completions_count))
+
+### Get completions of a prefix
+
+Use `GetCompletions(app_id, prefix_text)`. Example:
+
+```sql
+CALL GetCompletions(1, 'a');
+```
+**Time complexity:** O(log(table_prefixes_count × table_completions_count))
---
+++ initialize.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 19, 2020 at 09:45 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `autocomplete`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE PROCEDURE `GetCompletions` (IN `app_id` INT, IN `prefix_text` VARCHAR(255))  BEGIN
+    IF (SELECT EXISTS (SELECT 1 FROM `prefixes` WHERE `prefix` = prefix_text)) THEN
+        SELECT `completion` FROM `completions`
+            WHERE `prefix_id` = (SELECT `id` FROM `prefixes` WHERE `app_id` = app_id AND `prefix` = prefix_text)
+            ORDER BY `rank` DESC;
+    END IF;
+END$$
+
+CREATE PROCEDURE `InsertCompletion` (IN `app_id` INT, IN `completion_text` VARCHAR(255), IN `max_prefixes` INT, IN `bucket_size_limit` INT)  BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE prefix VARCHAR(255) DEFAULT '';
+    DECLARE completion_length VARCHAR(255);
+    SET completion_length = CHAR_LENGTH(completion_text);
+    IF completion_length < max_prefixes THEN
+        SET max_prefixes = completion_length;
+    END IF;
+    insertion_loop: LOOP
+        IF i > max_prefixes THEN 
+            LEAVE insertion_loop;
+        END IF;
+        SET prefix = CONCAT(prefix, SUBSTRING(completion_text, i, 1));
+        INSERT INTO `prefixes` (`app_id`, `prefix`) VALUES (app_id, prefix)
+            ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+        SELECT LAST_INSERT_ID() INTO @prefix_id;
+        IF (SELECT EXISTS (SELECT 1 FROM `completions`
+            WHERE `prefix_id` = @prefix_id AND `completion` = completion_text)) THEN
+            UPDATE `completions` SET `rank` = `rank` + 1
+                WHERE `prefix_id` = @prefix_id AND `completion` = completion_text;
+        ELSE
+            SET @bucket_size = (SELECT COUNT(*) FROM `completions`
+                WHERE `prefix_id` = @prefix_id);
+            IF @bucket_size >= bucket_size_limit THEN
+                UPDATE `completions` SET `completion` = completion_text, `rank` = `rank` + 1
+                    WHERE `id` = (SELECT id FROM `completions`
+                        WHERE `prefix_id` = @prefix_id ORDER BY `rank` ASC LIMIT 1);
+            ELSE
+                INSERT INTO `completions` (`prefix_id`, `completion`, `rank`)
+                    VALUES (@prefix_id, completion_text, 1);
+            END IF;
+        END IF;
+        SET i = i + 1;
+    END LOOP;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apps`
+--
+
+CREATE TABLE `apps` (
+  `id` int(11) NOT NULL,
+  `app_key` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `completions`
+--
+
+CREATE TABLE `completions` (
+  `id` int(11) NOT NULL,
+  `prefix_id` int(11) NOT NULL,
+  `completion` varchar(255) NOT NULL,
+  `rank` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prefixes`
+--
+
+CREATE TABLE `prefixes` (
+  `id` int(11) NOT NULL,
+  `app_id` int(11) NOT NULL,
+  `prefix` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `apps`
+--
+ALTER TABLE `apps`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `app_key` (`app_key`) USING BTREE;
+
+--
+-- Indexes for table `completions`
+--
+ALTER TABLE `completions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `prefix_id-completion` (`prefix_id`,`completion`) USING BTREE,
+  ADD KEY `prefix_id` (`prefix_id`) USING BTREE;
+
+--
+-- Indexes for table `prefixes`
+--
+ALTER TABLE `prefixes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `app_id-prefix` (`app_id`,`prefix`),
+  ADD KEY `prefix` (`prefix`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apps`
+--
+ALTER TABLE `apps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `completions`
+--
+ALTER TABLE `completions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prefixes`
+--
+ALTER TABLE `prefixes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
